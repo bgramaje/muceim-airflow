7de031a9aa8873d71601f2f6a7bee2436060f9df
bgramaje <boralbgra@gmail.com>
2026-01-11 18:10:50 +0100

    new changes

--- dags/utils/utils.py
+++ dags/utils/utils.py
@@ -164,12 +164,12 @@ class DuckLakeConnectionManager:
         con.execute(f"""
             CREATE OR REPLACE SECRET mitma_conf (
                 TYPE HTTP,
-                EXTRA_HTTP_HEADERS MAP {
+                EXTRA_HTTP_HEADERS MAP {{
                     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36',
                     'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8',
                     'Accept-Language': 'es-ES,es;q=0.9',
                     'Connection': 'keep-alive'
-                }
+                }}
             );
         """)
 
